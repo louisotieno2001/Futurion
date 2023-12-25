eb25f648267f2dce63debf29a2365358e6f00190
Louis2001 <louisotieno2001@gmail.com>
2023-12-25 16:49:44 +0300

    Remmoved flashing colors by commenting out colorChange.js code

--- app/app.js
+++ app/app.js
@@ -80,7 +80,7 @@ app.post('/student', async (req, res) => {
   }
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
